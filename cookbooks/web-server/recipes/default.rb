4d52261d9f9df64903a8b742e970453d226d0e25
Kameron Kenny <kameron.kenny@salesforce.com>
2018-04-20 17:25:07 -0400

    add default index

--- cookbooks/web-server/recipes/default.rb
+++ cookbooks/web-server/recipes/default.rb
@@ -9,3 +9,12 @@ package 'httpd'
 service 'httpd' do
   action [:start, :enable]
 end
+
+file '/var/www/html/index.html' do
+  owner 'root'
+  group 'root'
+  mode 0644
+  action :create
+  content "#{node[:name]}"
+end
+
