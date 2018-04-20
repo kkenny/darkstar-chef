42af78a9dbaaf6bb561525cbdf1669b7129ff047
Kameron Kenny <kameron.kenny@salesforce.com>
2018-04-20 17:25:40 -0400

    bump cookbook version

--- cookbooks/web-server/metadata.rb
+++ cookbooks/web-server/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures web-server'
 long_description 'Installs/Configures web-server'
-version '0.1.0'
+version '0.1.1'
 chef_version '>= 12.14' if respond_to?(:chef_version)
 
 supports 'centos'
