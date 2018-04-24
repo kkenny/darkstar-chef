bf99b940dcc75cbba78e102acc184dba89403f1a
Kameron Kenny <kameron.kenny@salesforce.com>
2018-04-24 15:14:56 -0400

    do not forget the dependencies...

--- cookbooks/lb_master/metadata.rb
+++ cookbooks/lb_master/metadata.rb
@@ -8,6 +8,8 @@ version '0.1.0'
 chef_version '>= 12.14' if respond_to?(:chef_version)
 
 depends 'keepalived'
+depends 'haproxy'
+depends 'default-packages'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
