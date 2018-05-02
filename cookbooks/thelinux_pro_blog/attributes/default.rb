be9b5cd8e692bc9b57916c052ea8f9a9498eccf2
Kameron Kenny <kkenny@beast.local>
2018-05-01 23:03:54 -0400

    try without rbenv activated

--- cookbooks/thelinux_pro_blog/attributes/default.rb
+++ cookbooks/thelinux_pro_blog/attributes/default.rb
@@ -3,4 +3,3 @@ default['jekyll']['repository']		= 'https://github.com/kkenny/thelinux_pro.git'
 default['jekyll']['user']		= 'root'
 default['jekyll']['group']		= 'root'
 default['jekyll']['domain_name']	= 'thelinux.pro'
-default['jekyll']['rbenv']['activated']	= true
