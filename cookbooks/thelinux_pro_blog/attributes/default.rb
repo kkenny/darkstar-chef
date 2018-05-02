71b500ce905d5384327e78ef9470c1c3a7bd518e
Kameron Kenny <kameron.kenny@salesforce.com>
2018-05-02 01:07:55 -0400

    jsut build, do not serve

--- cookbooks/thelinux_pro_blog/attributes/default.rb
+++ cookbooks/thelinux_pro_blog/attributes/default.rb
@@ -3,6 +3,6 @@ default['jekyll']['repository']		= 'https://github.com/kkenny/thelinux_pro.git'
 default['jekyll']['user']		= 'root'
 default['jekyll']['group']		= 'root'
 default['jekyll']['domain_name']	= 'thelinux.pro'
-default['jekyll']['options']		= ['serve', '--detach']
 default['jekyll']['packages']		= ['git', 'rubygem-bundler', 'ruby-devel', 'gcc' ]
 default['jekyll']['command']		= '/usr/local/bin/jekyll'
+default['jekyll']['options']		= ['build']
