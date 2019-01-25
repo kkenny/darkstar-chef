ae56eaac6a60e1f6a1c376464b283ad7d2a26012
Kameron Kenny <kameron.kenny@salesforce.com>
2019-01-25 15:45:42 -0500

    add creds... this is insecure. :(

--- cookbooks/pulp-server/recipes/default.rb
+++ cookbooks/pulp-server/recipes/default.rb
@@ -59,7 +59,7 @@ file '/etc/pulp-db-managed' do
 end
 
 execute 'pulp-admin-login' do
-  command 'pulp-admin login'
+  command 'pulp-admin login -u admin -p admin'
   creates '/root/.pulp/user-cert.pem'
   action :run
 end
