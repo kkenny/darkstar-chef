36b694af698154d40dfce723d3ece207447cb56b
Kameron Kenny <kameron.kenny@salesforce.com>
2019-01-25 14:05:26 -0500

    to_s

--- cookbooks/pulp-server/recipes/default.rb
+++ cookbooks/pulp-server/recipes/default.rb
@@ -79,7 +79,7 @@ repos.each do |repo|
   bag = data_bag_item('pulp_repos', repo)
 
   execute 'create_repo' do
-    command "pulp-admin rpm repo create --repo-id #{bag['id']} --display-name #{bag['display_name']} --description \"#{bag['description']}\" --feed #{bag['feed']} --serve-http #{bag['serve_http'].to_string} --serve-https #{bag['serve_https'].to_string}"
+    command "pulp-admin rpm repo create --repo-id #{bag['id']} --display-name #{bag['display_name']} --description \"#{bag['description']}\" --feed #{bag['feed']} --serve-http #{bag['serve_http'].to_s} --serve-https #{bag['serve_https'].to_s}"
     action :run
     notifies :run, 'execute[initialize-repo-content]', :immediately
     only_if bag['enabled'].true
