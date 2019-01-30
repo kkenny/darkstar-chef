c01769eb72e41899e4aea3a536bc40a57aa6f53a
Kameron Kenny <kameron.kenny@salesforce.com>
2019-01-30 14:48:44 -0500

    strange things

--- cookbooks/pulp-mirror/recipes/main-master.rb
+++ cookbooks/pulp-mirror/recipes/main-master.rb
@@ -9,16 +9,37 @@ repos = data_bag('pulp_library')
 repos.each do |repo|
   bag = data_bag_item('pulp_library', repo)
 
-  pulp_rpm_repo bag['id'] do
-    display_name bag['display_name']
-    description bag['description']
-    feed bag['feed']
-    http bag['serve_http']
-    https bag['serve_https']
-    pulp_cert_verify false
-    relative_url bag['relative_url']
-    action [:create, :sync] if bag['enabled']
-    notifies :touch, file["#{Chef::Config['file_cache_path']}/#{bag['id']}.sync"], :immediately
+  # This feels *really* strange
+  if bag['enabled']
+    file "#{Chef::Config['file_cache_path']}/#{bag['id']}.created" do
+      action :create
+      notifies :create, pulp_rpm_repos[bag['id']], :immediately
+    end
+
+    file "#{Chef::Config['file_cache_path']}/#{bag['id']}.synced" do
+      action :touch if file_age("#{Chef::Config['file_cache_path']}/#{bag['id']}.sync") > node['pulp-mirror']['sync']['cadence'] || not File.exist?("#{Chef::Config['file_cache_path']}/#{bag['id']}.sync")
+      notifies :sync, pulp_rpm_repos[bag['id']], :immediately
+    end
+
+    file "#{Chef::Config['file_cache_path']}/#{bag['id']}.published" do
+      action :create
+      notifies :publish, pulp_rpm_repos[bag['id']], :immediately
+    end
+  end
+
+  if not bag['enabled']
+    file "#{Chef::Config['file_cache_path']}/#{bag['id']}.created" do
+      action :delete
+      notifies :delete, pulp_rpm_repos[bag['id']], :immediately
+    end
+
+    file "#{Chef::Config['file_cache_path']}/#{bag['id']}.synced" do
+      action :delete
+    end
+
+    file "#{Chef::Config['file_cache_path']}/#{bag['id']}.published" do
+      action :delete
+    end
   end
 
   pulp_rpm_repo bag['id'] do
@@ -29,28 +50,6 @@ repos.each do |repo|
     https bag['serve_https']
     pulp_cert_verify false
     relative_url bag['relative_url']
-    action :sync if file_age("#{Chef::Config['file_cache_path']}/#{bag['id']}.sync") > node['pulp-mirror']['sync']['cadence']
-    notifies :touch, file["#{Chef::Config['file_cache_path']}/#{bag['id']}.sync"], :immediately
-  end
-
-  pulp_rpm_repo bag['id'] do
-    display_name bag['display_name']
-    description bag['description']
-    feed bag['feed']
-    http bag['serve_http']
-    https bag['serve_https']
-    pulp_cert_verify false
-    relative_url bag['relative_url']
-    action :publish if bag['enabled']
-    not_if { File.exist?("#{Chef::Config['file_cache_path']}/#{bag['id']}.published") }
-    notifies :touch, file["#{Chef::Config['file_cache_path']}/#{bag['id']}.published"], :immediately
-  end
-
-  file "#{Chef::Config['file_cache_path']}/#{bag['id']}.sync" do
-    action :nothing
-  end
-
-  file "#{Chef::Config['file_cache_path']}/#{bag['id']}.published" do
     action :nothing
   end
 end
