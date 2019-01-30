0285eda8f31a1f862bc7450c0bdaca0072f6c5d4
Kameron Kenny <kameron.kenny@salesforce.com>
2019-01-30 12:14:25 -0500

    ssl stuff

--- cookbooks/pulp-mirror/attributes/default.rb
+++ cookbooks/pulp-mirror/attributes/default.rb
@@ -3,3 +3,15 @@ default['pulp_server']['install_mongodb']	= true
 default['pulp_server']['install_qpid']		= true
 default['pulp_server']['configure_repos']	= true
 default['pulp_server']['configure_epel']	= true
+
+default['pulp_server']['config']['database']	= {
+  'verify_ssl' => false
+}
+
+default['pulp_server']['config']['servier']	= {
+  'verify_ssl' => false
+}
+
+default['pulp_server']['admin']['server']	= {
+  'verify_ssl' => false
+}
