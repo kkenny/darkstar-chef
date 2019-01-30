b6301a4b8ea360f85272a1293b935e7b06eaad38
Kameron Kenny <kameron.kenny@salesforce.com>
2019-01-30 12:54:15 -0500

    update relative url

--- cookbooks/pulp-mirror/recipes/default.rb
+++ cookbooks/pulp-mirror/recipes/default.rb
@@ -1,1 +1,13 @@
 include_recipe 'pulp_server::default'
+
+execute 'pulp-gen-key-pair' do
+  command 'pulp-gen-key-pair'
+  creates '/etc/pki/pulp/rsa.key'
+  action :run
+end
+
+execute 'pulp-gen-ca-certificate' do
+  command 'pulp-gen-ca-certificate'
+  creates '/etc/pki/pulp/ca.crt'
+  action :run
+end
